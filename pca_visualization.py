5fef426b4e0adf9e08888ad2f69017e4017aa19a
Julian Lee <julianlee@linkervision.com>
2024-07-20 16:44:57 +0800

    pca_visualization

--- pca_visualization.py
+++ pca_visualization.py
@@ -1,60 +1,73 @@
 # %%
 import torch
 import os
-
-
-feat_dim_dict = {'s': 384, 'b': 768, 'l': 1024, 'g': 1536}
-# model_size in ['s', 'b', 'l', 'g']
-model_size = 'b' 
-
-
-feat_dim = feat_dim_dict[model_size]
-backbone = torch.hub.load('facebookresearch/dinov2', f'dinov2_vit{model_size}14').cuda()
-
-
-# %%
 import copy
-import torch
 from ultralytics.nn.tasks import yaml_model_load, parse_model
 
-def update_ssl_backbone(yolo_model, ssl_path):
+
+
+# %% 
+use_vit = False
+if use_vit:
+    # settings for ViT
+    feat_dim_dict = {'s': 384, 'b': 768, 'l': 1024, 'g': 1536}
+    # model_size in ['s', 'b', 'l', 'g']
+    model_size = 'b' 
+    feat_dim = feat_dim_dict[model_size]
+    backbone = torch.hub.load('facebookresearch/dinov2', f'dinov2_vit{model_size}14').cuda()
+    image_size = 518
+    patch_size = backbone.patch_size # patchsize=14
+
+else:
+    # settings for YOLO
+    yolo_path = '/home/julian/work/dinov2/ultralytics/ultralytics/cfg/models/v8/yolov8n-ssl.yaml'
+    # ssl_path = '/mnt/data-home/julian/tiip/dinov2/imangenet-v0.1-0712/model_final.pt'
+    ssl_path = '/home/julian/yolov8n-scratch.pt'
+    yolo_yaml = yaml_model_load(yolo_path) 
+    feat_dim = 256
+    image_size = 640
+    patch_size = 32
+
+    def update_ssl_backbone(yolo_model, ssl_state_dict, prefix):
+        updated_count = 0
+        unupdated_count = 0
+        for key in yolo_model.state_dict():
+            ssl_key = prefix + key
+            if ssl_key in ssl_state_dict:
+                # print(f'{ssl_key} in ssl model')
+                updated_count += 1
+                yolo_model.state_dict()[key].copy_(ssl_state_dict[ssl_key])
+            else:
+                print(f'{ssl_key} not in ssl model')
+                unupdated_count += 1
+        print(f'{updated_count=} {unupdated_count=}')
+
+    
+    ch = 3
+    # model, save = parse_model(copy.deepcopy(yaml), ch=ch, verbose=True)  # model, savelist
+    yolo_model, _ = parse_model(copy.deepcopy(yolo_yaml), ch=ch, verbose=True)
     ssl_model = torch.load(ssl_path)
+
+
     print(f"updating backbone {ssl_path}")
-    updated_count = 0
-    unupdated_count = 0
-    for key in yolo_model.state_dict():
-        ssl_key = 'teacher.backbone.' + key
-        if ssl_key in ssl_model['model']:
-            # print(f'{ssl_key} in ssl model')
-            updated_count += 1
-            yolo_model.state_dict()[key].copy_(ssl_model['model'][ssl_key])
-        else:
-            print(f'{ssl_key} not in ssl model')
-            unupdated_count += 1
-    print(f'{updated_count=} {unupdated_count=}')
-
-
-yolo_path = '/home/julian/work/dinov2/ultralytics/ultralytics/cfg/models/v8/yolov8n-ssl.yaml'
-ssl_path = '/mnt/data-home/julian/tiip/dinov2/imangenet-v0.1-0712/model_final.pt'
-yolo_yaml = yaml_model_load(yolo_path) 
-ch = 3
-# model, save = parse_model(copy.deepcopy(yaml), ch=ch, verbose=True)  # model, savelist
-
-yolo_model, _ = parse_model(copy.deepcopy(yolo_yaml), ch=ch, verbose=True)
-update_ssl_backbone(yolo_model, ssl_path)
-backbone = yolo_model
+    ssl_model = torch.load(ssl_path)
+    # ssl_state_dict = ssl_model['model']
+    # prefix = 'teacher.'
+    ssl_state_dict = ssl_model['model'].state_dict()
+    prefix = 'model.'
+    update_ssl_backbone(yolo_model, ssl_state_dict, prefix=prefix)
+    backbone = yolo_model
+    backbone.cuda()
 
 
 # %% cell 6
 # this threshold depends on model size, and not sure the criterion is < or >
 # currently tested < for b, g, > for l, s
-background_treshold = 0.35 # smaller, fewer background
-image_size = 518
+background_threshold = 0.3 # smaller, fewer background
+smaller = False
 # folder_path = '/home/julian/work/Dino_V2/harryported_giffin_images'
-# folder_path = '/mnt/data-home/julian/tiip/images/kaohsiung5gsmartcitydemo/pca-examples-s1'
+# folder_path = '/mnt/data-home/julian/ssl/pca-examples/tiip-s1'
 folder_path = '/mnt/data-home/julian/ssl/pca-examples/fashion'
-patch_size = backbone.patch_size # patchsize=14
-# patch_size = 26
 
 
 from PIL import Image
@@ -89,10 +102,13 @@ with torch.no_grad():
         img_path = os.path.join(folder_path, img_path)
         img = Image.open(img_path).convert('RGB')
         img_t = transform1(img)
-    
-        features_dict = backbone.forward_features(img_t.unsqueeze(0).cuda())
+        if use_vit:
+            features_dict = backbone.forward_features(img_t.unsqueeze(0).cuda())
+        else:
+            features_dict = backbone(img_t.unsqueeze(0).cuda())
         features = features_dict['x_norm_patchtokens']
         total_features.append(features)
+
 
 total_features = torch.cat(total_features, dim=0).cpu().detach()
 print('*'*50)
@@ -141,7 +157,10 @@ plt.show()
 
 #  cell 10
 # segment/seperate the backgound and foreground using the first component
-pca_features_bg = pca_features[:, 0] > background_treshold # from first histogram
+if smaller:
+    pca_features_bg = pca_features[:, 0] < background_threshold # from first histogram
+else:
+    pca_features_bg = pca_features[:, 0] > background_threshold # from first histogram
 pca_features_fg = ~pca_features_bg
 
 # plot the pca_features_bg
