787640b86bc97dc69308dbcd4f288e58182c990c
julianleelinker <julianlee@linkervision.com>
2024-07-22 17:34:56 +0800

    Fintune scrpit and conifg

--- fine_tune_all.py
+++ fine_tune_all.py
@@ -2,19 +2,13 @@ from ultralytics import YOLO
 
 
 # backbone_path = '/mnt/data-home/julian/tiip/dinov2/test0712/model_final.pth' # 197
-
 # backbone_path = '/home/julian/data/ssl/tiip-yolov8s-v0.1.1-0719/model_final.pth'
 backbone_path = '/home/julian/data/ssl/tiip-yolov8m-v0.1.1-0720/model_final.pth'
 # backbone_path = '/home/julian/data/ssl/tiip-v0.1-0716/model_final.pth'
 data_yaml = '/home/julian/work/ultralytics/ultralytics/cfg/datasets/tiip-scratch_yolo-s1-s19_s21.yaml'
-
-# the model_path was saved by following code
-#with self.path_manager.open(save_file, "wb") as f:
-#    torch.save(data, cast(IO[bytes], f))
+pretrain_path = '/home/julian/work/ultralytics/runs/detect/tiip-yolov8m-v0.1.1-0721-finetune/weights/best.pt'
 
 # load the model
-# model = torch.load(model_path)
-model = YOLO("yolov8m.yaml")
-model.model.update_backbone(backbone_path)
+model = YOLO("yolov8m.yaml").load(pretrain_path)
 
-results = model.train(data=data_yaml, epochs=100, imgsz=640, name='tiip-yolov8m-v0.1.1-0721', batch=64, freeze=9)
+results = model.train(data=data_yaml, epochs=100, imgsz=640, name='tiip-yolov8m-v0.1.1-0722-all-finetune', batch=32)
---
+++ train_scratch.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+data_yaml = '/home/julian/work/ultralytics/ultralytics/cfg/datasets/tiip-scratch_yolo-s1-s19_s21.yaml'
+
+# load the model
+# model = torch.load(model_path)
+model = YOLO("yolov8m.yaml")
+
+results = model.train(data=data_yaml, epochs=100, imgsz=640, name='scratch-yolov8m-0722', batch=32)
