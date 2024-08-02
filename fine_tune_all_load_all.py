de0bdf13a065c74789eeba528f556b72e7bf12f5
Julian Lee <julianlee@linkervision.com>
2024-08-02 18:25:11 +0800

    fix script

--- fine_tune_all_load_all.py
+++ fine_tune_all_load_all.py
@@ -5,9 +5,12 @@ from ultralytics import YOLO
 data_yaml = '/home/julian/work/dinov2/ultralytics/ultralytics/cfg/datasets/tiip-scratch_yolo-s1-s22.yaml'
 # pretrain_path = '/home/julian/work/ultralytics/runs/detect/tiip-yolov8m-v0.1.1-0721-finetune/weights/best.pt'
 # pretrain_path = '/home/julian/work/ultralytics/runs/detect/tiip-yolov8m-v0.1.2-0722-2/weights/best.pt'
-pretrain_path = '/home/julian/work/ultralytics/runs/detect/tiip-yolov8m-v0.1.2-0726-resize-freeze/weights/best.pt'
+# pretrain_path = '/home/julian/work/ultralytics/runs/detect/tiip-yolov8m-v0.1.2-0726-resize-freeze/weights/best.pt'
+# pretrain_path = '/home/julian/work/ultralytics/runs/detect/tiip-yolov8m-v0.1.2-0726-resize-freeze/weights/best.pt'
+pretrain_path = '/home/julian/work/ultralytics/runs/detect/tiip-yolov8m-v0.1.2-0722-2-larger-head/weights/best.pt'
 
 # load the model
-model = YOLO("yolov8m.yaml").load(pretrain_path)
+model = YOLO("yolov8s.yaml").load(pretrain_path)
 
-results = model.train(data=data_yaml, epochs=100, imgsz=640, name='tiip-yolov8m-v0.1.2-0726-all-finetune', batch=32)
+# results = model.train(data=data_yaml, epochs=100, imgsz=640, name='tiip-yolov8m-v0.1.2-0726-all-finetune', batch=32)
+results = model.train(data=data_yaml, epochs=100, imgsz=640, name='tiip-yolov8m-v0.1.2-0722-2-larger-head-0731-finetune', batch=32)
