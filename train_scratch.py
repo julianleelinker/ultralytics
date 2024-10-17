91f12ff3e2b8e2d49a9ac08b78c71e992a49cceb
Julian Lee <julianlee@linkervision.com>
2024-10-17 16:21:28 +0800

    Add BDD-YOLO.yaml data config

--- train_scratch.py
+++ train_scratch.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
 
 # data_yaml = '/home/julian/work/ultralytics/ultralytics/cfg/datasets/tiip-scratch_yolo-s1-s19_s21.yaml' # leo
-data_yaml = '/home/julian/work/dinov2/ultralytics/ultralytics/cfg/datasets/tiip-scratch_yolo-s1-s22.yaml'
+# data_yaml = '/home/julian/work/dinov2/ultralytics/ultralytics/cfg/datasets/tiip-scratch_yolo-s1-s22.yaml'
+data_yaml = './ultralytics/cfg/datasets/BDD-YOLO.yaml'
 
 # load the model
 # model = torch.load(model_path)
 model = YOLO("yolov8m.yaml")
 
-results = model.train(data=data_yaml, epochs=100, imgsz=640, name='scratch-yolov8m-0722', batch=32)
+results = model.train(data=data_yaml, epochs=100, imgsz=640, name='BDD-YOLO-test', batch=32)
