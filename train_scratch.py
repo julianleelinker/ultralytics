0fc6b23043ed01624a4b18a4d6fba862aa3cc4c7
Julian Lee <julianlee@linkervision.com>
2024-10-21 11:35:45 +0800

    Update dataset configs

--- train_scratch.py
+++ train_scratch.py
@@ -2,10 +2,13 @@ from ultralytics import YOLO
 
 # data_yaml = '/home/julian/work/ultralytics/ultralytics/cfg/datasets/tiip-scratch_yolo-s1-s19_s21.yaml' # leo
 # data_yaml = '/home/julian/work/dinov2/ultralytics/ultralytics/cfg/datasets/tiip-scratch_yolo-s1-s22.yaml'
-data_yaml = './ultralytics/cfg/datasets/BDD-YOLO.yaml'
+# data_yaml = './ultralytics/cfg/datasets/BDDdataset-YOLO.yaml'
+# data_yaml = './ultralytics/cfg/datasets/waymo-YOLO.yaml'
+# data_yaml = './ultralytics/cfg/datasets/nuimage-YOLO.yaml'
+data_yaml = './ultralytics/cfg/datasets/mobility-YOLO.yaml'
 
 # load the model
 # model = torch.load(model_path)
 model = YOLO("yolov8m.yaml")
 
-results = model.train(data=data_yaml, epochs=100, imgsz=640, name='BDD-YOLO-test', batch=32)
+results = model.train(data=data_yaml, epochs=100, imgsz=640, name='mobility-YOLO', batch=48, device=[2])
